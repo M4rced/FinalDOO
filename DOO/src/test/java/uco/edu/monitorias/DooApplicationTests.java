2f64a59177a94d172721cf9ed2e958bd9759d3ef
M4rced <marcedlll12@gmail.com>
2022-11-17 12:14:28 -0500

    Final

---
+++ DOO/src/test/java/uco/edu/monitorias/DooApplicationTests.java
@@ -0,0 +1,13 @@
+package uco.edu.monitorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
